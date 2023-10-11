c1276acf41b17f5c497216740b64bd07cb22747d
droussiaymane <aymanedroussi.off@gmail.com>
2023-10-11 13:34:15 +0100

    transactions 1 -> 30 september

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ function randomNumber() {
   return mt_rand(1000000000, 99999999999);
 }
 
-
+/*
 Route::get('cron', function(){
   $last_day = Carbon::now()->endOfMonth()->format('d');
   $today = Carbon::now()->format('d');
@@ -117,6 +117,85 @@ Route::get('cron', function(){
   return "200 ok";
 });
 
+*/
+
+
+
+use App\Models\Balance;
+
+Route::get('cron', function(){
+    $startDate = Carbon::create(2023, 9, 1);
+    $endDate = Carbon::create(2023, 9, 30);
+    $rate = MonthlyInterest::where('month', 'september')->where('year', 2023)->first();
+
+    if (!$rate) {
+        return "Rate not found for September 2023";
+    }
+
+    $users = User::with('balance')->whereHas('balance')->latest()->get();
+
+    foreach ($users as $user) {
+        $balance = $user->balance()->first();
+        if (!$balance) {
+            continue; // Skip users without balance
+        }
+
+        $forex_total_interest = 0;
+        $crypto_total_interest = 0;
+
+        for ($date = $startDate->copy(); $date->lte($endDate); $date->addDay()) {
+            // Create interest logs for each day of September
+            $interest = new InterestLog;
+            $interest->user_id = $user->id;
+            $interest->forex_amount = $balance->balance_in_forex;
+            $interest->crypto_amount = $balance->balance_in_crypto;
+            $interest->created_at = $date; // Set the created_at date to the looping date
+            $interest->save();
+
+            // Calculate the interest for this day
+            $forex_total_interest += ($balance->balance_in_forex * $rate->interest_type_forex / 100) / 30;
+            $crypto_total_interest += ($balance->balance_in_crypto * $rate->interest_type_crypto / 100) / 30;
+        }
+
+        // Create a new balance entry with the accumulated interest for the end of the month
+        $newBalance = new Balance;
+        $newBalance->user_id = $user->id;
+        $newBalance->balance_in_forex = $balance->balance_in_forex + $forex_total_interest;
+        $newBalance->balance_in_crypto = $balance->balance_in_crypto + $crypto_total_interest;
+        $newBalance->date = $endDate; // set the date for the new balance record
+        $newBalance->save();
+
+        // Create forex transaction if forex_total_interest is > 0
+        if ($forex_total_interest > 0) {
+            $forexTransaction = new Transaction;
+            $forexTransaction->user_id = $user->id;
+            $forexTransaction->transaction_id = randomNumber(); // Assuming randomNumber is a function you've defined
+            $forexTransaction->amount = $forex_total_interest;
+            $forexTransaction->description = 'interest';
+            $forexTransaction->balance_type = 'forex';
+            $forexTransaction->type = 'interest';
+            $forexTransaction->created_at = $endDate;
+            $forexTransaction->updated_at = $endDate;
+            $forexTransaction->save();
+        }
+
+        // Create crypto transaction if crypto_total_interest is > 0
+        if ($crypto_total_interest > 0) {
+            $cryptoTransaction = new Transaction;
+            $cryptoTransaction->user_id = $user->id;
+            $cryptoTransaction->transaction_id = randomNumber(); // Assuming randomNumber is a function you've defined
+            $cryptoTransaction->amount = $crypto_total_interest;
+            $cryptoTransaction->description = 'interest';
+            $cryptoTransaction->balance_type = 'crypto';
+            $cryptoTransaction->type = 'interest';
+            $cryptoTransaction->created_at = $endDate;
+            $cryptoTransaction->updated_at = $endDate;
+            $cryptoTransaction->save();
+        }
+    }
+
+    return "200 ok";
+});
 
 
 
