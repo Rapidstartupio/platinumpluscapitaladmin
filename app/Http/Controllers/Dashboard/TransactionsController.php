25f4e9680309b3a71edd4514db1a6f90028f4a69
droussiaymane <aymanedroussi.off@gmail.com>
2023-10-11 15:30:20 +0100

    showing transactions for the authenticated user

--- app/Http/Controllers/Dashboard/TransactionsController.php
+++ app/Http/Controllers/Dashboard/TransactionsController.php
@@ -3,28 +3,38 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Http\Request;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
 
 class TransactionsController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function index() {
-        if (Auth::user() == null)
-        {
-            return redirect()->route('login');
-        }
-        $transactions = Transaction::orderBy('created_at', 'desc')->paginate(10);
+        // Get the logged in user's ID
+        $userId = Auth::id();
+
+        // Fetch only transactions of the authenticated user
+        $transactions = Transaction::where('user_id', $userId)
+                          ->orderBy('created_at', 'desc')
+                          ->paginate(10);
+
         return view('dashboard.transaction.transactions', compact('transactions'));
     }
 
     public function view_transaction($id) {
-        if (Auth::user() == null)
-        {
-            return redirect()->route('login');
+        // Fetch only transactions of the authenticated user
+        $transaction = Transaction::where('transaction_id', '=', $id)
+                          ->where('user_id', Auth::id())
+                          ->first();
+
+        if (!$transaction) {
+            return redirect()->route('dashboard.transaction.transactions')
+                             ->with('error', 'Transaction not found or not accessible.');
         }
-        $transaction = Transaction::where('transaction_id', '=', $id)->first();
+
         return view('dashboard.transaction.view_transaction', compact('transaction'));
     }
 }
